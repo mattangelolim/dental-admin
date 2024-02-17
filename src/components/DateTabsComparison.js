bf188224faf9c1af0b9dbe8545480a4bf43fe582
unknown <kylemendoza67@gmail.com>
2024-02-17 22:22:28 +0800

    revenue in progress

--- src/components/DateTabsComparison.js
+++ src/components/DateTabsComparison.js
@@ -1,4 +1,5 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -49,6 +50,186 @@ export default function DateTabsComparison({ data }) {
     setValue(newValue);
   };
 
+  const [dailyData, setDailyData] = useState([]);
+  const [weeklyData, setWeeklyData] = useState([]);
+  const [monthlyData, setMonthlyData] = useState([]);
+  const [yearlyData, setYearlyData] = useState([]);
+
+  useEffect(() => {
+    const fetchDaily = async () => {
+      try {
+        const response = await axios.get(
+          "https://13.211.204.176/revenue/analytics?filter=daily"
+        );
+        console.log(response.data);
+        setDailyData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    const fetchWeekly = async () => {
+      try {
+        const response = await axios.get(
+          "https://13.211.204.176/revenue/analytics?filter=weekly"
+        );
+        console.log(response.data);
+        setWeeklyData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    const fetchMonthly = async () => {
+      try {
+        const response = await axios.get(
+          "https://13.211.204.176/revenue/analytics?filter=monthly"
+        );
+        console.log(response.data);
+        setMonthlyData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    const fetchYearly = async () => {
+      try {
+        const response = await axios.get(
+          "https://13.211.204.176/revenue/analytics?filter=yearly"
+        );
+        console.log(response.data);
+        setYearlyData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchDaily();
+    fetchWeekly();
+    fetchMonthly();
+    fetchYearly();
+  }, []);
+
+  const formattedDailyData = {
+    labels:
+      // dailyData && dailyData.revenueData
+      //   ? dailyData.revenueData.map((item) => item.date)
+      //   : [],
+      Array.from(
+        {
+          length:
+            dailyData && dailyData.revenueData
+              ? dailyData.revenueData.length
+              : 0,
+        },
+        (_, index) => `Day ${index + 1}`
+      ),
+    datasets: [
+      {
+        label: dailyData && dailyData.revenueData ? "Revenue" : "No Data",
+        data:
+          dailyData && dailyData.revenueData
+            ? dailyData.revenueData.map((item) => item.revenue)
+            : [],
+        backgroundColor: Array.from(
+          {
+            length:
+              dailyData && dailyData.revenueData
+                ? dailyData.revenueData.length
+                : 0,
+          },
+          (_, index) => "hsl(120, 70%, 50%)"
+        ),
+      },
+    ],
+  };
+
+  const formattedWeeklyData = {
+    labels: Array.from(
+      {
+        length:
+          weeklyData && weeklyData.revenueData
+            ? weeklyData.revenueData.length
+            : 0,
+      },
+      (_, index) => `Week ${index + 1}`
+    ),
+    datasets: [
+      {
+        label: weeklyData && weeklyData.revenueData ? "Revenue" : "No Data",
+        data:
+          weeklyData && weeklyData.revenueData
+            ? weeklyData.revenueData.map((item) => item.revenue)
+            : [],
+        backgroundColor: Array.from(
+          {
+            length:
+              weeklyData && weeklyData.revenueData
+                ? weeklyData.revenueData.length
+                : 0,
+          },
+          (_, index) => "hsl(120, 70%, 50%)"
+        ),
+      },
+    ],
+  };
+
+  const formattedMonthlyData = {
+    labels: Array.from(
+      {
+        length:
+          monthlyData && monthlyData.revenueData
+            ? monthlyData.revenueData.length
+            : 0,
+      },
+      (_, index) => `Month ${index + 1}`
+    ),
+    datasets: [
+      {
+        label: monthlyData && monthlyData.revenueData ? "Revenue" : "No Data",
+        data:
+          monthlyData && monthlyData.revenueData
+            ? monthlyData.revenueData.map((item) => item.revenue)
+            : [],
+        backgroundColor: Array.from(
+          {
+            length:
+              monthlyData && monthlyData.revenueData
+                ? monthlyData.revenueData.length
+                : 0,
+          },
+          (_, index) => "hsl(120, 70%, 50%)"
+        ),
+      },
+    ],
+  };
+
+  const formattedYearlyData = {
+    labels: Array.from(
+      {
+        length:
+          yearlyData && yearlyData.revenueData
+            ? yearlyData.revenueData.length
+            : 0,
+      },
+      (_, index) => `Year ${index + 1}`
+    ),
+    datasets: [
+      {
+        label: yearlyData && yearlyData.revenueData ? "Revenue" : "No Data",
+        data:
+          yearlyData && yearlyData.revenueData
+            ? yearlyData.revenueData.map((item) => item.revenue)
+            : [],
+        backgroundColor: Array.from(
+          {
+            length:
+              yearlyData && yearlyData.revenueData
+                ? yearlyData.revenueData.length
+                : 0,
+          },
+          (_, index) => "hsl(120, 70%, 50%)"
+        ),
+      },
+    ],
+  };
+
   return (
     <Box sx={{ width: "100%" }}>
       <Box sx={{ borderBottom: 1 }}>
@@ -71,22 +252,22 @@ export default function DateTabsComparison({ data }) {
       </Box>
       <CustomTabPanel value={value} index={0}>
         <div className=" h-[30rem] ">
-          <ReactJsBarChart data={data} />
+          <ReactJsBarChart data={formattedDailyData} />
         </div>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         <div className=" h-[30rem]">
-          <ReactJsBarChart data={data} />
+          <ReactJsBarChart data={formattedWeeklyData} />
         </div>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={2}>
         <div className=" h-[30rem]">
-          <ReactJsBarChart data={data} />
+          <ReactJsBarChart data={formattedMonthlyData} />
         </div>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={3}>
         <div className=" h-[30rem]">
-          <ReactJsBarChart data={data} />
+          <ReactJsBarChart data={formattedYearlyData} />
         </div>
       </CustomTabPanel>
     </Box>
